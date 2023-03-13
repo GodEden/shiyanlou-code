2022e20a55573b4450e72da0d943405f32615979
Your Name <you@example.com>
2023-03-14 05:56:41 +0800

    test

---
+++ Jump7.java
@@ -0,0 +1,13 @@
+public class Jump7{
+    public static void main(String[] args){
+        int i = 0;
+        while(i < 100) {
+            i++;    
+            if(i % 7 == 0 || i % 10 == 7 || i / 10 == 7) {
+                continue;            
+            }
+            System.out.println(i);    
+        }
+    }
+}
+   
